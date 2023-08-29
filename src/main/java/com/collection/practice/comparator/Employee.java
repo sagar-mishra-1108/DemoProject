27a3893026228ed33570e10c0434e291f2713dd4
Sagar <sagar.mishra.110890@gmail.com>
2023-08-29 22:04:42 +0100

    Sorting with comparator by primitive and stream

---
+++ src/main/java/com/collection/practice/comparator/Employee.java
@@ -0,0 +1,27 @@
+package com.collection.practice.comparator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class Employee {
+    private Integer id;
+    private String name;
+    private Double salary;
+    private Designation designation;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", designation='" + designation + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/collection/practice/comparator/EmployeeNameAgeDesignationSalaryComparator.java
@@ -0,0 +1,21 @@
+package com.collection.practice.comparator;
+
+import java.util.Comparator;
+
+public class EmployeeNameAgeDesignationSalaryComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        int nameComparison = emp1.getName().compareTo(emp2.getName());
+        int ageComparison = emp1.getAge() - emp2.getAge();
+        int designationComparison = emp1.getDesignation().getRank() - emp2.getDesignation().getRank();
+        int salaryComparison = Double.compare(emp1.getSalary(), emp2.getSalary());
+        return nameComparison == 0
+                ? ageComparison == 0
+                    ? designationComparison == 0
+                        ? salaryComparison
+                        : designationComparison
+                    : ageComparison
+                : nameComparison;
+
+    }
+}
